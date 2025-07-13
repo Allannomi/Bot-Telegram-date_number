8d06176eb0e36856ef6699b80d1bded358d75d9e
Allan Nunes <137460915+Allannomi@users.noreply.github.com>
2025-07-13 19:13:03 -0300

    Update data_random.py

--- data_random.py
+++ data_random.py
@@ -4,10 +4,6 @@ import random
 def data():
     agora = datetime.datetime.now()
     data_agora = agora.strftime("%d/%m/%Y às %H:%M:%S")
-    """
-    SEMPRE TEM QUE SER RETURN
-    O BOT DO TELEGRAM NÃO USA PRINT, TEM QUE RETORNAR A INFORMAÇÃO, NAO PRINTAR
-    """
     return data_agora
 
 def aleatorio():
